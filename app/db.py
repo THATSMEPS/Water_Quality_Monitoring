33ee6bdf4fffc081a056e7452dbf17a74dedc8d3
THATSMEPS <pspratham1234@gmail.com>
2025-04-05 16:42:38 +0530

    Day1 by Pratham

---
+++ app/db.py
@@ -0,0 +1,7 @@
+import motor.motor_asyncio
+import os
+from dotenv import load_dotenv
+load_dotenv()
+MONGO_URI = os.getenv("MONGO_URI")
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+database = client["water_quality"]
