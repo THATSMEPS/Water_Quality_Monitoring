bb6701bfc9127a4ec8396b5adf5ba2e804d6d8a6
THATSMEPS <pspratham1234@gmail.com>
2025-04-25 11:50:08 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './style.css'; 
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
-import Analytics from './pages/Analytics';
 import SensorData from './pages/SensorData';
 import DataManagement from './pages/DataManagement';
 import Alerts from './pages/Alerts';
+
 function App() {
   return (
     <Router>
@@ -15,7 +15,7 @@ function App() {
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/analytics" element={<Analytics />} />
+            
             <Route path="/sensordata" element={<SensorData />} />
             <Route path="/datamanagement" element={<DataManagement />} />
             <Route path="/alerts" element={<Alerts />} />
