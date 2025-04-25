dd687e260dee7781982cb3f80e89030f7cebd798
THATSMEPS <pspratham1234@gmail.com>
2025-04-25 11:50:54 +0530

    Update Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,7 +8,7 @@ function Sidebar() {
       <h2>Water Dashboard</h2>
       <ul>
         <li><Link to="/">🚰Home</Link></li>
-        <li><Link to="/analytics">📊 Analytics</Link></li>
+        
         <li><Link to="/sensordata">📡 Sensor Data</Link></li>
         <li><Link to="/alerts">🚨 Alerts</Link></li>
         <li><Link to="/datamanagement">🛠️ Data Management</Link></li>
